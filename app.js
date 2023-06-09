9f4c60cd168fc5f82770aa1a29da6a5d2d68b929
igoldclasher <45177759+igoldclasher@users.noreply.github.com>
2023-06-09 23:48:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ btn6.addEventListener("click", function(){
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(tg.initDataUnsafe);
+    tg.sendData(item);
 });
 
 let usercard = document.getElementById("usercard");
