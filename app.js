5da5047ece3dc9cd0ae7aa812c29851b26bb1925
igoldclasher <45177759+igoldclasher@users.noreply.github.com>
2023-06-09 18:28:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ btn6.addEventListener("click", function(){
 });
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function (){
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(item);
 });
 
