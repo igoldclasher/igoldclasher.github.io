830c3edb65bf03c646ced625ba2eff539fa1acb9
igoldclasher <45177759+igoldclasher@users.noreply.github.com>
2023-06-09 18:20:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.TextColor = "#FFFFFF";
-tg.MainButton.color = "2cab37";
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
 
 let item= "";
 
